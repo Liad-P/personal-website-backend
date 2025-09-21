eada77957bd006ee5e8b481950e9aefd2b19263a
Liad Peretz <peretz.liad@gmail.com>
2025-09-21 12:16:43 +0200

    Added support for configuring the context documents through environment config.

--- src/main/java/liad/dev/ai/services/AIService.java
+++ src/main/java/liad/dev/ai/services/AIService.java
@@ -1,10 +1,12 @@
 package liad.dev.ai.services;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -23,7 +25,10 @@ public class AIService {
     @Inject
     DocumentLoader documentLoader;
 
-    final List<String> documents = Arrays.asList("Liad-Peretz-CV.pdf");
+    final static String DEFAULT_DOCUMENT = "Liad-CV.pdf";
+    
+    @ConfigProperty(name = "CONTEXT_DOCUMENT_IDS", defaultValue = DEFAULT_DOCUMENT)
+    Optional<List<String>> documents;
 
     Map<String, IChatSession> chatSessions = new HashMap<>();
 
@@ -35,10 +40,21 @@ public class AIService {
 
     public IChatSession initializeChatSession(String sessionID) {
         var chatSession = aiClient.createChatSession(sessionID);
-        this.documents.forEach(document -> {
+        this.documents.get().forEach(document -> {
             try {
-                ByteBuffer pdfByteBuffer = documentLoader.load(document);
-                chatSession.addPDF(pdfByteBuffer.array());
+                if (document == null || document.isEmpty()) {
+                    // Make sure there's always at least one document to load
+                    Log.warn("No document ID provided to load context from.");
+                    document = DEFAULT_DOCUMENT.split(",")[0];
+                }
+                ByteBuffer documentByteBuffer = documentLoader.load(document);
+                if (document.endsWith(".md")) {
+                    Log.info("Adding markdown document to chat session: " + document);
+                    chatSession.addMarkdown(documentByteBuffer.array());
+                } else if (document.endsWith(".pdf")) {
+                    Log.info("Adding PDF document to chat session: " + document);
+                    chatSession.addPDF(documentByteBuffer.array());
+                }
             } catch (DocumentNotFound e) {
                 Log.error("Document not found: " + document + ". Error: " + e.getMessage(), e);
             }
